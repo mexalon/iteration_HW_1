a840676de1c5ed54ebe041fc064a980e1a0eae4e
Baryshnikov Nikolay <nabarysh@gmail.com>
2021-03-12 21:35:00 +0300

    second

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import os
+import hashlib
 from stuff.config import *
 import json
 
 sf = source_file_path
 tf = target_file_path
+hf = hash_file_path
 wiki_url = target_url
 
 
 class Countries:
-    def __init__(self, from_file: str, to_file: str, to_url, start=None, stop=None):
+    def __init__(self, from_file: str):
         self.from_file = from_file
-        self.to_file = to_file
         self.idx = -1
-        if start:
-            self.idx = start
-
-        self.stop = stop
-        self.to_url = to_url
+        self.stop = False
+        self.to_url = None
+        self.to_file = None
         self.one_land = None
 
     def __iter__(self):
@@ -26,36 +25,29 @@ class Countries:
         self.idx += 1
         self.read_country()
         if self.stop:
-            if self.idx >= self.stop:
-                raise StopIteration
+            self.idx = -1
+            self.stop = False
+            raise StopIteration
 
         return self.idx
 
     def __str__(self):
-        return f'{self.one_land}'
+        return f'{self.idx + 1}: {self.one_land}'
 
-    def read_country(self, my_idx=None):
-        idx = self.idx
-        if my_idx:
-            idx = my_idx
-
+    def read_country(self):
         with open(self.from_file, 'r', encoding='UTF-8') as s:
             try:
-                self.one_land = json.load(s)[idx]['name']['official']
+                self.one_land = json.load(s)[self.idx]['name']['official']
             except IndexError:
-                self.stop = idx
+                self.stop = True
 
         return self.one_land
 
-    def write_country(self, my_idx=None):
-        idx = self.idx
-        if my_idx:
-            idx = my_idx
-
+    def write_country(self, to_file: str, to_url):
         if self.one_land:
-            with open(self.to_file, 'a', encoding='UTF-8') as t:
-                url = f"[{self.one_land}]({self.to_url + self.one_land.strip().replace(' ', '_')})"
-                t.write(f"{idx + 1}. {url}\n")
+            with open(to_file, 'a', encoding='UTF-8') as t:
+                url = f"[{self.one_land}]({to_url + self.one_land.strip().replace(' ', '_')})"
+                t.write(f"{self.idx + 1}. {url}\n")
 
 
 def del_file(file_name: str):
@@ -65,9 +57,34 @@ def del_file(file_name: str):
         os.remove(path)
 
 
-if __name__ == '__main__':
-    del_file(tf)
-    ctf = Countries(sf, tf, wiki_url)
+def hw_1(source_filename: str, target_filename: str, url):
+    """Первое задание"""
+    del_file(target_filename)
+    ctf = Countries(source_filename)
     for country in ctf:
         print(ctf)
-        ctf.write_country()
+        ctf.write_country(target_filename, url)
+
+
+def my_generator(some_file: str):
+    """Генератор хэшей"""
+    with open(some_file, 'rb') as f:
+        line = f.readline()
+        while line:
+            one_hash = hashlib.md5(line).hexdigest()
+            yield one_hash
+            line = f.readline()
+
+
+def hw_2(source_filename: str, target_filename: str):
+    """Второе задание"""
+    del_file(target_filename)
+    for item in my_generator(source_filename):
+        print(item)
+        with open(target_filename, 'a') as h:
+            h.write(f'{item}\n')
+
+
+if __name__ == '__main__':
+    hw_1(sf, tf, wiki_url)
+    hw_2(tf, hf)
--- stuff/config.py
+++ stuff/config.py
@@ -1,3 +1,4 @@
 source_file_path = 'countries.json'
 target_file_path = 'result.md'
+hash_file_path = 'result_hash.txt'
 target_url = 'https://en.wikipedia.org/wiki/'
